964cfd755e507efee9266ba4c22637cdc54324c0
julianadenauer <github@julianadenauer.de>
2016-06-13 22:29:54 +0200

    imprint and analytics

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$( document ).ready(function() {
+  // smooth scrolling for internal links from here: http://www.paulund.co.uk/smooth-scroll-to-internal-links-with-jquery
+  $('a.smooth').on('click',function (e) {
+      e.preventDefault();
+      var target = this.hash,
+      $target = $(target);
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 900, 'swing', function () {
+          window.location.hash = target;
+      });
+  });
+
+  var menuSpeed = 150;
+
+  $("#showMenu").click(function(){
+    $("#menuoverlay").animate({width:'toggle'},menuSpeed);
+    return false;
+  });
+
+  $("#hideMenu").click(function(){
+    $("#menuoverlay").animate({width:'toggle'},menuSpeed);
+    return false;
+  });
+
+  $("#menuoverlay ul a").click(function(){
+    $("#menuoverlay").animate({width:'toggle'},menuSpeed);
+  });
+});
--- js/zange.js
+++ js/zange.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
 var container;
